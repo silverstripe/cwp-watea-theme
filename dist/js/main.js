e8fe9c4df942ee2d213660334f6486213f5b9061
adunn <adunn@silverstripe.com>
2018-12-03 09:23:23 +1300

    sd-13: Fixed laravel mix config issue

--- dist/js/main.js
+++ dist/js/main.js
@@ -33,9 +33,6 @@
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
 /******/
-/******/ 	// identity function for calling harmony imports with the correct context
-/******/ 	__webpack_require__.i = function(value) { return value; };
-/******/
 /******/ 	// define getter function for harmony exports
 /******/ 	__webpack_require__.d = function(exports, name, getter) {
 /******/ 		if(!__webpack_require__.o(exports, name)) {
@@ -60,10 +57,10 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "./";
+/******/ 	__webpack_require__.p = "/";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 6);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -74,14 +71,22 @@ module.exports = jQuery;
 
 /***/ }),
 /* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(2);
+module.exports = __webpack_require__(6);
+
+
+/***/ }),
+/* 2 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_bootstrap_accessibility_plugin_plugins_js_bootstrap_accessibility__ = __webpack_require__(5);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_bootstrap_accessibility_plugin_plugins_js_bootstrap_accessibility__ = __webpack_require__(3);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_bootstrap_accessibility_plugin_plugins_js_bootstrap_accessibility___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_bootstrap_accessibility_plugin_plugins_js_bootstrap_accessibility__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__components_carousel__ = __webpack_require__(3);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__components_navigation__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__components_carousel__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__components_navigation__ = __webpack_require__(5);
 /* eslint-disable */
 /**
  * The PayPal Accessibility Plugin adds accessibility improvements over the top of Bootstrap
@@ -95,77 +100,14 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 
-__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__components_carousel__["a" /* default */])();
-__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_2__components_navigation__["a" /* default */])();
-
-/***/ }),
-/* 2 */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
+Object(__WEBPACK_IMPORTED_MODULE_1__components_carousel__["a" /* default */])();
+Object(__WEBPACK_IMPORTED_MODULE_2__components_navigation__["a" /* default */])();
 
 /***/ }),
 /* 3 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports) {
 
-"use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_jquery__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_jquery___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_jquery__);
-/* eslint-disable */
-
-/* eslint-enable */
-
-/* harmony default export */ __webpack_exports__["a"] = (function () {
-  // Carousel Documentation: https://paypal.github.io/bootstrap-accessibility-plugin/demo.html#carousel
-
-  var $carousel = __WEBPACK_IMPORTED_MODULE_0_jquery___default()('.carousel');
-  var $carouselPause = __WEBPACK_IMPORTED_MODULE_0_jquery___default()('#carousel-pause');
-  var $carouselPlay = __WEBPACK_IMPORTED_MODULE_0_jquery___default()('#carousel-play');
-
-  $carousel.carousel({
-    wrap: true,
-    interval: 10000
-  });
-
-  $carouselPause.click(function () {
-    $carousel.carousel('pause');
-    $carouselPause.hide();
-    $carouselPlay.show().focus();
-  });
-
-  $carouselPlay.click(function () {
-    $carousel.carousel('cycle');
-    $carouselPlay.hide();
-    $carouselPause.show().focus();
-  });
-});
-
-/***/ }),
-/* 4 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_jquery__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_jquery___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_jquery__);
-/* eslint-disable */
-
-/* eslint-enable */
-
-/* harmony default export */ __webpack_exports__["a"] = (function () {
-  /**
-   * Unbind the space bar handler that is added in the bootstrap-accessibility plugin,
-   * the functionality is recreated in this component.
-   *
-   * See: bootstrap-accessibility.js, line 130 (ish)
-   */
-  __WEBPACK_IMPORTED_MODULE_0_jquery___default()(document).off('keydown.bs.dropdown.data-api', '[data-toggle=dropdown], [role=menu]', __WEBPACK_IMPORTED_MODULE_0_jquery___default.a.fn.dropdown.Constructor.prototype.keydown);
-});
-
-/***/ }),
-/* 5 */
-/***/ (function(module, exports, __webpack_require__) {
-
-/* WEBPACK VAR INJECTION */(function(jQuery) {/* ========================================================================
+/* ========================================================================
 * Extends Bootstrap v3.1.1
 
 * Copyright (c) <2014> eBay Software Foundation
@@ -572,15 +514,69 @@ __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_2__components_navigation__["a" /
 
 })(jQuery);
 
-/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
+
+/***/ }),
+/* 4 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_jquery__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_jquery___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_jquery__);
+/* eslint-disable */
+
+/* eslint-enable */
+
+/* harmony default export */ __webpack_exports__["a"] = (function () {
+  // Carousel Documentation: https://paypal.github.io/bootstrap-accessibility-plugin/demo.html#carousel
+
+  var $carousel = __WEBPACK_IMPORTED_MODULE_0_jquery___default()('.carousel');
+  var $carouselPause = __WEBPACK_IMPORTED_MODULE_0_jquery___default()('#carousel-pause');
+  var $carouselPlay = __WEBPACK_IMPORTED_MODULE_0_jquery___default()('#carousel-play');
+
+  $carousel.carousel({
+    wrap: true,
+    interval: 10000
+  });
+
+  $carouselPause.click(function () {
+    $carousel.carousel('pause');
+    $carouselPause.hide();
+    $carouselPlay.show().focus();
+  });
+
+  $carouselPlay.click(function () {
+    $carousel.carousel('cycle');
+    $carouselPlay.hide();
+    $carouselPause.show().focus();
+  });
+});
+
+/***/ }),
+/* 5 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_jquery__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_jquery___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_jquery__);
+/* eslint-disable */
+
+/* eslint-enable */
+
+/* harmony default export */ __webpack_exports__["a"] = (function () {
+  /**
+   * Unbind the space bar handler that is added in the bootstrap-accessibility plugin,
+   * the functionality is recreated in this component.
+   *
+   * See: bootstrap-accessibility.js, line 130 (ish)
+   */
+  __WEBPACK_IMPORTED_MODULE_0_jquery___default()(document).off('keydown.bs.dropdown.data-api', '[data-toggle=dropdown], [role=menu]', __WEBPACK_IMPORTED_MODULE_0_jquery___default.a.fn.dropdown.Constructor.prototype.keydown);
+});
 
 /***/ }),
 /* 6 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-__webpack_require__(1);
-module.exports = __webpack_require__(2);
-
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
